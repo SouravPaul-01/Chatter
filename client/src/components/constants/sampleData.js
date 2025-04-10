559927add87bca3879d519d15d92a8ac7f0b8b6a
sourav <souravpaul01032002@gmail.com>
2025-04-10 13:46:00 +0530

    Worked on ChatList,Will work on Avatar creation later.

---
+++ client/src/components/constants/sampleData.js
@@ -0,0 +1,22 @@
+export const sampleChats = [
+  {
+    id: 1,
+    avatar: [
+      "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=250&d=mp",
+    ],
+    name: "John Smith",
+    _id: "1",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+  {
+    id: 2,
+    avatar: [
+      "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=250&d=mp",
+    ],
+    name: "John Boi",
+    _id: "2",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+];
