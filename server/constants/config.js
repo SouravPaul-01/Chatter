5fc599b1857f9292c561a3030271adb8c229f2d2
Sourav Paul <110439418+SouravPaul-01@users.noreply.github.com>
2025-05-21 15:21:16 +0530

    Fixing deploy bugs

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,9 +1,19 @@
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "http://localhost:4173",
+//     process.env.CLIENT_URL,
+//   ],
+//   credentials: true,
+// };
+const allowedOrigins = ["http://localhost:5173", "http://localhost:4173"];
+
+if (process.env.CLIENT_URL) {
+  allowedOrigins.push(process.env.CLIENT_URL);
+}
+
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
-  ],
+  origin: allowedOrigins,
   credentials: true,
 };
 
